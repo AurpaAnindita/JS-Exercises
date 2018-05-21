a8dc20886ab42072e347269b595730bf4f65b717
Aurpa Anindita <464438anindita@gmail.com>
2018-05-21 04:55:50 +0000

    Task 4

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ console.log(sumArray([1,2,3,4,5]));
 console.log(sumArray([1,20,-3,45]));
 
 //Task 3
+
+
 function checkEmail(emailString){
     var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     var match = emailFormat.test(emailString);
@@ -22,3 +24,26 @@ function checkEmail(emailString){
 }
 console.log(checkEmail("john@smith.com"));
 console.log(checkEmail("johnsmith.com"));
+ 
+//Task 4
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+function getReadingStatus(index)
+{
+    return library[index].readingStatus;
+}
+console.log(getReadingStatus(1));
